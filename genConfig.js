2752b45c362c312c06a7b8ab8496449f99f5cd9e
Krishna Mani <krishnamani314@gmail.com>
2020-12-04 16:53:32 -0800

    change db_url to db_string

--- genConfig.js
+++ genConfig.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 
 const data = JSON.parse(fs.readFileSync('./configBase.json'));
 data.BOT_TOKEN = process.argv[2];
-data.db_url = process.argv[3];
+data.db_string = process.argv[3];
 fs.writeFileSync('./config.json', JSON.stringify(data));
